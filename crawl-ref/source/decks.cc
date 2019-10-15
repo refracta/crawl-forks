42078651b4bc7c31332c8c33fcf086acad118989
Aidan Holm <aidanholm@gmail.com>
2019-10-15 21:09:41 +0800

    Add ui-specific event class
    Mostly necessitated by the need to add an event target member.
    
    Different event types are now implemented as subclasses. Unfortunately
    this means downcasting via static_cast<> in some cases, although the new
    on_foo_event() handlers remove that need by passing a derived event.
    
    I'm not too sure about the Event::Type::Foo verbosity... otherwise, it's
    mostly a net improvement. In C++20 you can do `using enum Event::Type;`
    which might help, when we get C++20.

--- crawl-ref/source/decks.cc
+++ crawl-ref/source/decks.cc
@@ -845,7 +845,7 @@ static void _describe_cards(vector<card_type> cards)
     auto popup = make_shared<ui::Popup>(scroller);
 
     bool done = false;
-    popup->on_keydown_event([&done, &scroller](const wm_event& ev) {
+    popup->on_keydown_event([&done, &scroller](const KeyEvent& ev) {
         done = !scroller->on_event(ev);
         return true;
     });
