85280d6ba93e62fcf202bb9611ca9183ca06e096
Aidan Holm <aidanholm@gmail.com>
2019-11-10 19:55:02 +0800

    Partially restore backspace-clears-name behaviour (advil, 12119)
    Fully restoring the old behaviour is non-trivial, but this change should
    make clearing the initial name easy, which is the primary use-case.
    Backspace will now clear if it's the first key pressed, and delete one
    character at a time otherwise.

--- crawl-ref/source/startup.cc
+++ crawl-ref/source/startup.cc
@@ -681,6 +681,7 @@ private:
     string input_string;
     vector<player_save_info> chars;
     int num_saves;
+    bool first_action = true;
 
     bool button_event_hook(const wm_event& ev, MenuButton* btn)
     {
@@ -834,6 +835,12 @@ void UIStartupMenu::on_show()
         }
         else if (keyn == CK_BKSP)
         {
+            if (first_action)
+            {
+                first_action = false;
+                input_string = "";
+                changed_name = true;
+            }
             if (!input_string.empty())
             {
                 input_string.erase(input_string.size() - 1);
