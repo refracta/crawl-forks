yrmvgh/yiufcrawl

new attack flavor and some other plutonium crab tweaks

crawl-ref/source/mon-util.cc
@@ -2085,7 +2085,8 @@ bool flavour_triggers_damageless(attack_flavour flavour)
         || flavour == AF_DROWN
         || flavour == AF_CORRODE
         || flavour == AF_HUNGER
-		|| flavour == AF_CLEAVE;
+		|| flavour == AF_CLEAVE
+		|| flavour == AF_CONTAM;
 }
 
 /**

refracta/crawl-forks

crawl-ref/source/mon-util.cc
@@ -2241,7 +2241,8 @@ bool flavour_triggers_damageless(attack_flavour flavour)
         || flavour == AF_DROWN
         || flavour == AF_CORRODE
         || flavour == AF_HUNGER
-        || flavour == AF_POISON_STR;
+        || flavour == AF_POISON_STR
+		|| flavour == AF_CONTAM;
 }
 
 /**