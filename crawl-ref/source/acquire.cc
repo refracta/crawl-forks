crawl/crawl

Fix Trog being unable to gift artefacts (#12159)
But restrict them to the same brand set as non-artefacts. Also prevent gifting
the staff of Olgreb (since other unrand enhancer staves are disallowed), and
unrand weapons of pain such as Morg. Other unrands can currently still be
gifted regardless of brand.

crawl-ref/source/acquire.cc
@@ -1162,8 +1162,15 @@ static void _adjust_brand(item_def &item, bool divine, int agent)
     if (is_artefact(item))
         return; // their own kettle of fish
 
-    if (agent == GOD_TROG)
-        return; // handled elsewhere
+
+    // Trog has a restricted brand table.
+    if (agent == GOD_TROG && item.base_type == OBJ_WEAPONS)
+    {
+        // 75% chance of a brand
+        item.brand = random_choose(SPWPN_NORMAL, SPWPN_VORPAL,
+                                   SPWPN_FLAMING, SPWPN_ANTIMAGIC);
+        return;
+    }
 
     // Not from a god, so we should prefer better brands.
     if (!divine && item.base_type == OBJ_WEAPONS)
@@ -1204,14 +1211,16 @@ static string _why_reject(const item_def &item, int agent)
     // Trog does not gift the Wrath of Trog, nor weapons of pain
     // (which work together with Necromantic magic).
     // nor fancy magic staffs (wucad mu, majin-bo, staff of battle, elem
-    // staff)
+    // staff, staff of olgreb)
     if (agent == GOD_TROG)
     {
         if (is_unrandom_artefact(item, UNRAND_TROG)
             || is_unrandom_artefact(item, UNRAND_WUCAD_MU)
             || is_unrandom_artefact(item, UNRAND_MAJIN)
             || is_unrandom_artefact(item, UNRAND_BATTLE)
-            || is_unrandom_artefact(item, UNRAND_ELEMENTAL_STAFF))
+            || is_unrandom_artefact(item, UNRAND_ELEMENTAL_STAFF)
+            || is_unrandom_artefact(item, UNRAND_OLGREB)
+            || get_weapon_brand(item) == SPWPN_PAIN)
         {
             return "Destroying a weapon Trog hates!";
         }
@@ -1283,19 +1292,8 @@ int acquirement_create_item(object_class_type class_wanted,
         if (agent == GOD_TROG && !one_chance_in(3))
             want_arts = false;
 
-        int ego = 0;
-
-        // Trog has a restricted brand table for weapons; force the ego
-        // in the call to items()
-        if (agent == GOD_TROG && class_wanted == OBJ_WEAPONS)
-        {
-            // 75% chance of a brand
-            ego = random_choose(SPWPN_FORBID_BRAND, SPWPN_VORPAL,
-                                SPWPN_FLAMING, SPWPN_ANTIMAGIC);
-        }
-
         thing_created = items(want_arts, class_wanted, type_wanted,
-                              ITEM_LEVEL, ego, agent);
+                              ITEM_LEVEL, 0, agent);
 
         if (thing_created == NON_ITEM)
         {

crawl-ref/source/artefact.cc
@@ -130,9 +130,13 @@ static bool _god_fits_artefact(const god_type which_god, const item_def &item,
         break;
 
     case GOD_TROG:
-        // Anti-magic god: no spell use, no enhancing magic.
-        if (brand == SPWPN_PAIN) // Pain involves necromantic spell use.
+        // Limited selection of brands.
+        if (brand != SPWPN_VORPAL
+            && brand != SPWPN_FLAMING
+            && brand != SPWPN_ANTIMAGIC)
+        {
             return false;
+        }
 
         if (artefact_property(item, ARTP_MAGICAL_POWER) > 0)
             return false;

refracta/crawl-forks

crawl-ref/source/acquire.cc
@@ -1130,8 +1130,15 @@ static void _adjust_brand(item_def &item, bool divine, int agent)
     if (is_artefact(item))
         return; // their own kettle of fish
 
-    if (agent == GOD_TROG)
-        return; // handled elsewhere
+
+    // Trog has a restricted brand table.
+    if (agent == GOD_TROG && item.base_type == OBJ_WEAPONS)
+    {
+        // 75% chance of a brand
+        item.brand = random_choose(SPWPN_NORMAL, SPWPN_VORPAL,
+                                   SPWPN_MOLTEN, SPWPN_ANTIMAGIC);
+        return;
+    }
 
     // Not from a god, so we should prefer better brands.
     if (!divine && item.base_type == OBJ_WEAPONS)
@@ -1198,13 +1205,15 @@ static string _why_reject(const item_def &item, int agent)
     // Trog does not gift the Wrath of Trog, nor weapons of pain
     // (which work together with Necromantic magic).
     // nor fancy magic staffs (wucad mu, majin-bo, staff of battle, elem
-    // staff)
+    // staff, staff of olgreb)
     if (agent == GOD_TROG)
     {
         if (is_unrandom_artefact(item, UNRAND_TROG)
             || is_unrandom_artefact(item, UNRAND_WUCAD_MU)
             || is_unrandom_artefact(item, UNRAND_MAJIN)
-            || is_unrandom_artefact(item, UNRAND_ELEMENTAL_STAFF))
+            || is_unrandom_artefact(item, UNRAND_ELEMENTAL_STAFF)
+            || is_unrandom_artefact(item, UNRAND_OLGREB)
+            || get_weapon_brand(item) == SPWPN_PAIN)
         {
             return "Destroying a weapon Trog hates!";
         }
@@ -1276,19 +1285,8 @@ int acquirement_create_item(object_class_type class_wanted,
         if (agent == GOD_TROG && !one_chance_in(3))
             want_arts = false;
 
-        int ego = 0;
-
-        // Trog has a restricted brand table for weapons; force the ego
-        // in the call to items()
-        if (agent == GOD_TROG && class_wanted == OBJ_WEAPONS)
-        {
-            // 75% chance of a brand
-            ego = random_choose(SPWPN_FORBID_BRAND, SPWPN_VORPAL,
-                                SPWPN_MOLTEN, SPWPN_ANTIMAGIC);
-        }
-
         thing_created = items(want_arts, class_wanted, type_wanted,
-                              ITEM_LEVEL, ego, agent);
+                              ITEM_LEVEL, 0, agent);
 
         if (thing_created == NON_ITEM)
         {

crawl-ref/source/artefact.cc
@@ -130,11 +130,13 @@ static bool _god_fits_artefact(const god_type which_god, const item_def &item,
         break;
 
     case GOD_TROG:
-        // Anti-magic god: no spell use, no enhancing magic.
-        if (brand == SPWPN_PAIN) // Pain involves necromantic spell use.
+        // Limited selection of brands.
+        if (brand != SPWPN_VORPAL
+            && brand != SPWPN_MOLTEN
+            && brand != SPWPN_ANTIMAGIC)
+        {
             return false;
-        if (item.base_type == OBJ_STAVES)
-            return true;
+        }
 
         if (artefact_property(item, ARTP_MAGICAL_POWER) > 0)
             return false;