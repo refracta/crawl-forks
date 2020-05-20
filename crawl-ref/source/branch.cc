34068f595f199f17c8dc3f3fb7f2c4c156fd260f
Lucien <bhylton7@gmail.com>
2020-05-20 07:53:02 -0700

    Flag old ossuaries as "unfinished" (basically synonymous with "removed").

--- crawl-ref/source/branch.cc
+++ crawl-ref/source/branch.cc
@@ -278,7 +278,8 @@ bool branch_is_unfinished(branch_type branch)
     if (branch == BRANCH_DWARF
         || branch == BRANCH_FOREST
         || branch == BRANCH_BLADE
-        || branch == BRANCH_LABYRINTH)
+        || branch == BRANCH_LABYRINTH
+        || branch == BRANCH_OSSUARY)
     {
         return true;
     }
