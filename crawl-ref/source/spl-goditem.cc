7e8617d580bde222a7b57c0e5f4a0c59f8acde2a
Lucien <bhylton7@gmail.com>
2020-05-27 19:11:31 -0700

    Double Torm protection for players too, not just monsters.

--- crawl-ref/source/spl-goditem.cc
+++ crawl-ref/source/spl-goditem.cc
@@ -1066,7 +1066,7 @@ void torment_player(actor *attacker, torment_source_type taux)
     if (!player_res_torment())
     {
         // Negative energy resistance can alleviate torment.
-        hploss = max(0, you.hp * (50 - player_prot_life() * 5) / 100 - 1);
+        hploss = max(0, you.hp * (50 - player_prot_life() * 10) / 100 - 1);
         // Statue form is only partial petrification.
         if (you.form == transformation::statue || you.species == SP_GARGOYLE)
             hploss /= 2;
