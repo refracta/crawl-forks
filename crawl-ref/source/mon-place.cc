ecd55c82624ba7b3af51bc3a70e551ce66f7a3c5
Lucien <bhylton7@gmail.com>
2020-06-14 21:56:23 -0700

    Embed cheibrodos_lair_end_hotspot into a wall (orient tag) and make it use volcano tileset. Embed boggens_dragon_lair_wall into a wall (orient tag). Rename the new slime_altar_1 to slime_island and restore a simpler slime_altar_1. Finish up placing slimeshrooms and slime in slime entry vaults.

--- crawl-ref/source/mon-place.cc
+++ crawl-ref/source/mon-place.cc
@@ -103,7 +103,7 @@ static bool _feat_compatible(dungeon_feature_type wanted_feat,
     return wanted_feat == actual_feat
            || wanted_feat == DNGN_SLIMY_WATER && (feat_has_solid_floor(actual_feat) || feat_is_watery(actual_feat))
            || wanted_feat == DNGN_DEEP_WATER && feat_is_watery(actual_feat)
-           || wanted_feat == DNGN_FLOOR && feat_has_solid_floor(actual_feat) && actual_feat != DNGN_SLIMY_WATER
+           || wanted_feat == DNGN_FLOOR && feat_has_solid_floor(actual_feat) && (actual_feat != DNGN_SLIMY_WATER)
            || wanted_feat == DNGN_ROCK_WALL && actual_feat == DNGN_CLEAR_ROCK_WALL
            || wanted_feat == DNGN_METAL_WALL && actual_feat == DNGN_SILVER_WALL;
 }
