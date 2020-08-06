8aaa83c267554570f9d493c937559e7986c2d3dd
Lucien <bhylton7@gmail.com>
2020-08-06 01:05:56 -0700

    Revert "Remove the dragon slaying brand"
    This reverts commit 9b7421e1bc34b7e424dcfd497249e2e32f0a6e0c.

--- crawl-ref/source/player-equip.cc
+++ crawl-ref/source/player-equip.cc
@@ -563,6 +563,13 @@ static void _equip_weapon_effect(item_def& item, bool showMsgs, bool unmeld, equ
                         mpr("You see sparks fly.");
                     break;
 
+                case SPWPN_DRAGON_SLAYING:
+                    mpr(player_genus(GENPC_DRACONIAN)
+                        || you.form == TRAN_DRAGON
+                            ? "You feel a sudden desire to commit suicide."
+                            : "You feel a sudden desire to slay dragons!");
+                    break;
+
                 case SPWPN_VENOM:
                     mprf("%s begins to drip with poison!", item_name.c_str());
                     break;
