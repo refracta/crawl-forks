bf39bb4ab2593d5e44c20a3e4306b78eb3707b53
Lucien <bhylton7@gmail.com>
2020-09-27 00:43:58 -0700

    Altering sewer maps to allow them to be D:2 subvaults instead of encompass vaults.
    sewer_kobolds broken into: elyvilon_niche (kobold band with early Ely altar) and disused_pipe (ambush by sewer rats and crocs; but with quick stairs down if defeated).
    minmay_sewer_treatment reduced to just one of the treatment houses with tough bands inside.
    kennysheep_pipe_town retired (too similar to minmay_sewer_treatment).
    sewer_frog_island_01 and sewer_frog_island_02 shaved down in size to be better floats. Renamed sewer_frog_pond and sewer_frog_spiral. Ending monster set tougher.
    minmay_sewer_w trimmed down to minmay_sewer_ubend.
    All maps that end with one or two bullfrogs changed to end with two to four bullfrogs, spiny frogs, crocodiles and/or Prince Ribbit.
    (Bullfrog is a normal monster on the floor; so something at the end of a vault should be harder).
    sewer_big_baddy trimmed into purgy_house; featuring Purgy with some helpers (sewer rat band, hound or giant frog).

--- crawl-ref/source/dungeon.cc
+++ crawl-ref/source/dungeon.cc
@@ -1533,6 +1533,10 @@ static void _sewer_water()
 
     for (rectangle_iterator ri(coord_def(0, 0), coord_def(GXM - 1, GYM - 1)); ri; ++ri)
     {
+
+        if (map_masked(*ri, MMT_NO_POOL))
+            continue;
+
         if (_sewer_check(*ri))
         {
             if (ri->x == 0 || ri->x == 1 || ri->x == GXM - 1 || ri->x == GXM - 2)
