7990d1bb071e1f692a4b58236549bc14e17d2fe8
Lucien <bhylton7@gmail.com>
2021-02-26 23:06:38 -0500

    REALLY Properly reroll artefact names.

--- crawl-ref/source/item-prop.cc
+++ crawl-ref/source/item-prop.cc
@@ -1395,11 +1395,8 @@ void set_item_ego_type(item_def &item, int ego_type)
         rap[ARTP_BRAND] = (short)ego_type;
 
         // We need to reroll it's name and looks in case the base type changed.
-        if (!is_unrandom_artefact(item))
-        {
-            item.props[ARTEFACT_APPEAR_KEY].get_string() = make_artefact_name(item, true);
-            item.props[ARTEFACT_APPEAR_KEY].get_string() = make_artefact_name(item, false);
-        }
+        item.props[ARTEFACT_APPEAR_KEY].get_string() = make_artefact_name(item, true);
+        item.props[ARTEFACT_NAME_KEY].get_string()   = make_artefact_name(item, false);
 
         item.props[CHANGED_BRAND_KEY].get_bool() = true;
     }
