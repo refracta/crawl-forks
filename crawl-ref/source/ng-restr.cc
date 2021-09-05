d28e3044bd27d79ebde1e5737901c4af417dff42
Lucien <bhylton7@gmail.com>
2021-09-04 21:58:36 -0400

    Restrict the vinestalker background away from combos that could exist the same way as race + background and Vs itself (no VsVs, no CeVs since VsCe is allowed, etc.)

--- crawl-ref/source/ng-restr.cc
+++ crawl-ref/source/ng-restr.cc
@@ -63,8 +63,15 @@ static bool _banned_combination(job_type job, species_type species)
             return true;
         // fallthrough
     case JOB_VINE_STALKER:
-        if (species == SP_LIGNIFITE)
+        if (species == SP_HUMAN
+            || species == SP_CENTAUR
+            || species == SP_NAGA
+            || species == SP_MERFOLK
+            || species == SP_LIGNIFITE
+            || species == SP_VINE_STALKER)
+        {
             return true;
+        }
         // fallthrough
     case JOB_DEMONSPAWN:
         if (species_undead_type(species) == US_GHOST)
