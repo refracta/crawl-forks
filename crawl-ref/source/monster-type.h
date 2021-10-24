9d2692cce412a8ba1708657c9917a78367d8bc54
Lucien <bhylton7@gmail.com>
2021-10-23 23:42:20 -0400

    New vault only (harmless, decorative) monster: Puffer Fish Skeleton. Touch up Yred and Kiku overflow vaults . . . some are deadlier than before.

--- crawl-ref/source/monster-type.h
+++ crawl-ref/source/monster-type.h
@@ -572,9 +572,7 @@ enum monster_type                      // menv[].type
     MONS_IRON_IMP,
     MONS_SHADOW_IMP,
     MONS_RED_DEVIL,
-#if TAG_MAJOR_VERSION == 34
-    MONS_ROTTING_DEVIL,
-#endif
+    MONS_PUFFERFISH,
     MONS_HELLWING,
     MONS_SIXFIRHY,
     MONS_NEQOXEC,
